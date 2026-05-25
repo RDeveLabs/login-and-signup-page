ccb94378119c350dd046c601c9badde0cbdab1a5
RDeveLabs <rdevelopamd@gmail.com>
2026-05-25 20:46:07 +0700

    testing

--- src/main.js
+++ src/main.js
@@ -1,23 +1,51 @@
-document.getElementById('submit').addEventListener('click', async (e) => {
+const loginForm = document.getElementById("login");
+const signupForm = document.getElementById("signup");
+
+if (loginForm) {
+  loginForm.addEventListener("click", async (e) => {
     e.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    
-    try{
-        const form = new FormData();
-        form.append("username", username);
-        form.append("password", password);
-        const res = await fetch('https://belajar.rdevelabs.biz.id/data',{
-            method: 'POST',
-            body: form
-        })
+    try {
+      const form = new FormData();
+      form.append("username", username);
+      form.append("password", password);
+      const res = await fetch("https://belajar.rdevelabs.biz.id/data", {
+        method: "POST",
+        body: form,
+      });
 
-        const status = await res.text();
-        console.log(form);
-        console.log(status);
-    }catch(e){
-        console.log("Ada error: " + e);
+      const status = await res.text();
+      console.log(form);
+      console.log(status);
+    } catch (e) {
+      console.log("Ada error: " + e);
     }
-})
+  });
+}
+
+if (signupForm) {
+  signupForm.addEventListener("click", async (e) => {
+    e.preventDefault;
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const form = new FormData();
+      form.append("username", username);
+      form.append("password", password);
+
+      const res = await fetch("https://belajar.rdevelabs.biz.id/data", {
+        method: "POST",
+        body: form,
+      });
+
+      console.log(await res.text);
+    } catch (e) {
+      console.log("ada error : ", e);
+    }
+  });
+}
